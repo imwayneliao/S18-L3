d6b8a4f96469396c66e81df597bb4717a3736351
imwayneliao <imwayneliao@gmail.com>
2018-02-15 08:58:01 +0800

    add hero and monster special effect when attacked

--- js/index.js
+++ js/index.js
@@ -17,6 +17,24 @@ class BaseCharacter {
     if (this.hp <= 0) { 
       this.die();
     }
+    var _this = this;
+    var i = 1;
+    _this.id = setInterval(function() {
+    if (i == 1) {
+        _this.element.getElementsByClassName("effect-image")[0].style.display = "block";
+        _this.element.getElementsByClassName("hurt-text")[0].classList.add("attacked");
+        _this.element.getElementsByClassName("hurt-text")[0].textContent = damage;
+      }      
+      _this.element.getElementsByClassName("effect-image")[0].src = 'images/effect/blade/'+ i +'.png';
+      i++;
+    if (i > 8) {
+      _this.element.getElementsByClassName("effect-image")[0].style.display = "none";
+      _this.element.getElementsByClassName("hurt-text")[0].classList.remove("attacked");
+      _this.element.getElementsByClassName("hurt-text")[0].textContent = "";
+      clearInterval(_this.id);
+    }
+
+  }, 50);
   }
   die() {
     this.alive = false;
@@ -74,3 +92,49 @@ class Monster extends BaseCharacter {
 }
 var hero = new Hero("Bernard", 130, 30);
 var monster = new Monster("Skeleton", 130, 10);
+
+var rounds = 10;
+function endTurn() {
+  rounds--;
+  document.getElementById("round-num").textContent = rounds;
+  if (rounds < 1) {
+    // 「遊戲結束」空白區
+  }
+}
+function heroAttack() {
+  document.getElementsByClassName("skill-block")[0].style.display = "none";
+
+  setTimeout(function() {
+    hero.element.classList.add("attacking");
+    console.log("開打");
+    setTimeout(function() {
+      hero.attack(monster);
+      hero.element.classList.remove("attacking");
+    }, 500);
+  }, 100);
+  setTimeout(function() {
+    if (monster.alive) {
+      monster.element.classList.add("attacking");
+      setTimeout(function() {
+        monster.attack(hero);
+        monster.element.classList.remove("attacking");
+        endTurn();
+        if (hero.alive == false) {
+          // 「遊戲結束」空白區
+        } else {
+          document.getElementsByClassName("skill-block")[0].style.display = "block";
+        }
+      }, 500);
+    } else {
+      // 「遊戲結束」空白區
+    }
+  }, 1100);
+}
+
+function addSkillEvent() {
+  var skill = document.getElementById("skill");
+  skill.onclick = function() { 
+    heroAttack(); 
+  }
+}
+addSkillEvent();
